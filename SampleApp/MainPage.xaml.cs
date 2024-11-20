5ea4064f021be482cb142c3c8a39904f08d09ab5
Tate Sakai [HA] <tate.sakai@hawaiianair.com>
2024-11-19 15:00:10 -1000

    initial

---
+++ SampleApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Plugin.StoreReview;
+
+namespace SampleApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private async void RequestAppStoreReview(object sender, EventArgs e)
+    {
+        await RequestAppStoreReview();
+    }
+
+    private async Task RequestAppStoreReview()
+    {
+        ReviewStatus reviewStatus = ReviewStatus.Unknown;
+        reviewStatus = await CrossStoreReview.Current.RequestReview(true);
+    }
+}
+
+
